c97548ce75acd272b7be49c681a14cc9e90c00c6
Genco0 <87459800+Genco0@users.noreply.github.com>
2021-07-25 21:48:23 +0300

    Create password_reminder.py

---
+++ password_reminder.py
@@ -0,0 +1,8 @@
+# Password Reminder Assigment 
+user_name = "Genco06"
+password = "Ankara06"
+user2 = str(input("Lütfen Kullanıcı adını giriniz: "))
+if user_name == user2 :
+  print ("Merhaba, {}! şifren : {}".format(user2,password))
+else :
+  print("Merhaba, {}! Sonra Görüşürüz.".format(user2))
